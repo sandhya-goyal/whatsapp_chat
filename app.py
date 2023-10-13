ae4fdc554a0308db6f6f417254116fa757569818
sandhya-goyal <102228647+sandhya-goyal@users.noreply.github.com>
2023-10-13 16:52:39 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,319 @@
+# Importing modules
+import nltk
+import streamlit as st
+import re
+import preprocessor,helper
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# App title
+st.sidebar.title("Whatsapp Chat And Sentiment Analyzer")
+
+# VADER : is a lexicon and rule-based sentiment analysis tool that is specifically attuned to sentiments.
+nltk.download('vader_lexicon')
+
+# File upload button
+uploaded_file = st.sidebar.file_uploader("Choose a file")
+
+# Main heading
+st. markdown("<h1 style='text-align: center; color: grey;'>Whatsapp Chat And Sentiment Analyzer</h1>", unsafe_allow_html=True)
+
+if uploaded_file is not None:
+    
+    # Getting byte form & then decoding
+    bytes_data = uploaded_file.getvalue()
+    d = bytes_data.decode("utf-8")
+    
+    # Perform preprocessing
+    data = preprocessor.preprocess(d)
+    
+    # Importing SentimentIntensityAnalyzer class from "nltk.sentiment.vader"
+    from nltk.sentiment.vader import SentimentIntensityAnalyzer
+    
+    # Object
+    sentiments = SentimentIntensityAnalyzer()
+    
+    # Creating different columns for (Positive/Negative/Neutral)
+    data["po"] = [sentiments.polarity_scores(i)["pos"] for i in data["message"]] # Positive
+    data["ne"] = [sentiments.polarity_scores(i)["neg"] for i in data["message"]] # Negative
+    data["nu"] = [sentiments.polarity_scores(i)["neu"] for i in data["message"]] # Neutral
+    
+    # To indentify true sentiment per row in message column
+    def sentiment(d):
+        if d["po"] >= d["ne"] and d["po"] >= d["nu"]:
+            return 1
+        if d["ne"] >= d["po"] and d["ne"] >= d["nu"]:
+            return -1
+        if d["nu"] >= d["po"] and d["nu"] >= d["ne"]:
+            return 0
+
+    # Creating new column & Applying function
+    data['value'] = data.apply(lambda row: sentiment(row), axis=1)
+    
+    # User names list
+    user_list = data['user'].unique().tolist()
+    
+    # Sorting
+    user_list.sort()
+    
+    # Insert "Overall" at index 0
+    user_list.insert(0, "Overall")
+    
+    # Selectbox
+    selected_user = st.sidebar.selectbox("Show analysis wrt", user_list)
+    
+    if st.sidebar.button("Show Analysis"):
+        # Stats Area
+        num_messages, words, num_media_messages, num_links,num_deleted_messages = helper.fetch_stats2(selected_user,data)
+        st.title("Top Statistics")
+        col1, col2, col3, col4,col5 = st.columns(5)
+
+        with col1:
+            st.header("Total Messages")
+            st.title(num_messages)
+        with col2:
+            st.header("Total Words")
+            st.title(words)
+        with col3:
+            st.header("Media Shared")
+            st.title(num_media_messages)
+        with col4:
+            st.header("Links Shared")
+            st.title(num_links)
+        with col5:
+            st.header("Messages Deleted")
+            st.title(num_deleted_messages)    
+
+
+        # monthly timeline
+        st.title("Monthly Timeline")
+        timeline = helper.monthly_timeline2(selected_user,data)
+        fig,ax = plt.subplots()
+        ax.plot(timeline['time'], timeline['message'],color='green')
+        plt.xticks(rotation='vertical')
+        st.pyplot(fig)
+
+        # activity map
+        st.title('Activity Map')
+        col1,col2 = st.columns(2)
+
+        with col1:
+            st.header("Most busy day")
+            busy_day = helper.week_activity_map2(selected_user,data)
+            fig,ax = plt.subplots()
+            ax.bar(busy_day.index,busy_day.values,color='purple')
+            plt.xticks(rotation='vertical')
+            st.pyplot(fig)
+
+        with col2:
+            st.header("Most busy month")
+            busy_month = helper.month_activity_map2(selected_user, data)
+            fig, ax = plt.subplots()
+            ax.bar(busy_month.index, busy_month.values,color='orange')
+            plt.xticks(rotation='vertical')
+            st.pyplot(fig)
+
+        st.title("Weekly Activity Map")
+        user_heatmap = helper.activity_heatmap2(selected_user,data)
+        fig,ax = plt.subplots()
+        ax = sns.heatmap(user_heatmap)
+        st.pyplot(fig)
+
+        # finding the busiest users in the group(Group level)
+        if selected_user == 'Overall':
+            st.title('Most Busy Users')
+            x,new_df = helper.most_busy_users2(data)
+            fig, ax = plt.subplots()
+
+            col1, col2 = st.columns(2)
+
+            with col1:
+                ax.bar(x.index, x.values,color='red')
+                plt.xticks(rotation='vertical')
+                st.pyplot(fig)
+            with col2:
+                st.dataframe(new_df)
+
+        # WordCloud
+        st.title("Wordcloud")
+        df_wc = helper.create_wordcloud2(selected_user,data)
+        fig,ax = plt.subplots()
+        ax.imshow(df_wc)
+        st.pyplot(fig)
+
+        # most common words
+        most_common_df = helper.most_common_words2(selected_user,data)
+
+        fig,ax = plt.subplots()
+
+        ax.barh(most_common_df[0],most_common_df[1])
+        plt.xticks(rotation='vertical')
+
+        st.title('Most commmon words')
+        st.pyplot(fig)
+
+        # emoji analysis
+        emoji_df = helper.emoji_helper2(selected_user,data)
+        st.title("Emoji Analysis")
+
+        col1,col2 = st.columns(2)
+
+        with col1:
+            st.dataframe(emoji_df)
+        with col2:
+            fig,ax = plt.subplots()
+            ax.pie(emoji_df[1].head(),labels=emoji_df[0].head(),autopct="%0.2f")
+            st.pyplot(fig)
+            
+        #sentiment
+        # Percentage contributed
+        if selected_user == 'Overall':
+            col1,col2,col3 = st.columns(3)
+            with col1:
+                st.markdown("<h3 style='text-align: center; color: white;'>Most Positive Contribution</h3>",unsafe_allow_html=True)
+                x = helper.percentage(data, 1)
+                
+                # Displaying
+                st.dataframe(x)
+            with col2:
+                st.markdown("<h3 style='text-align: center; color: white;'>Most Neutral Contribution</h3>",unsafe_allow_html=True)
+                y = helper.percentage(data, 0)
+                
+                # Displaying
+                st.dataframe(y)
+            with col3:
+                st.markdown("<h3 style='text-align: center; color: white;'>Most Negative Contribution</h3>",unsafe_allow_html=True)
+                z = helper.percentage(data, -1)
+                
+                # Displaying
+                st.dataframe(z)
+
+
+        # Most Positive,Negative,Neutral User...
+        if selected_user == 'Overall':
+            
+            # Getting names per sentiment
+            x = data['user'][data['value'] == 1].value_counts().head(10)
+            y = data['user'][data['value'] == -1].value_counts().head(10)
+            z = data['user'][data['value'] == 0].value_counts().head(10)
+
+            col1,col2,col3 = st.columns(3)
+            with col1:
+                # heading
+                st.markdown("<h3 style='text-align: center; color: white;'>Most Positive Users</h3>",unsafe_allow_html=True)
+                
+                # Displaying
+                fig, ax = plt.subplots()
+                ax.bar(x.index, x.values, color='green')
+                plt.xticks(rotation='vertical')
+                st.pyplot(fig)
+            with col2:
+                # heading
+                st.markdown("<h3 style='text-align: center; color: white;'>Most Neutral Users</h3>",unsafe_allow_html=True)
+                
+                # Displaying
+                fig, ax = plt.subplots()
+                ax.bar(z.index, z.values, color='grey')
+                plt.xticks(rotation='vertical')
+                st.pyplot(fig)
+            with col3:
+                # heading
+                st.markdown("<h3 style='text-align: center; color: white;'>Most Negative Users</h3>",unsafe_allow_html=True)
+                
+                # Displaying
+                fig, ax = plt.subplots()
+                ax.bar(y.index, y.values, color='red')
+                plt.xticks(rotation='vertical')
+                st.pyplot(fig)
+
+        # WORDCLOUD......
+        col1,col2,col3 = st.columns(3)
+        with col1:
+            try:
+                # heading
+                st.markdown("<h3 style='text-align: center; color: white;'>Positive WordCloud</h3>",unsafe_allow_html=True)
+                
+                # Creating wordcloud of positive words
+                df_wc = helper.create_wordcloud(selected_user, data,1)
+                fig, ax = plt.subplots()
+                ax.imshow(df_wc)
+                st.pyplot(fig)
+            except:
+                # Display error message
+                st.image('error.webp')
+        with col2:
+            try:
+                # heading
+                st.markdown("<h3 style='text-align: center; color: white;'>Neutral WordCloud</h3>",unsafe_allow_html=True)
+                
+                # Creating wordcloud of neutral words
+                df_wc = helper.create_wordcloud(selected_user, data,0)
+                fig, ax = plt.subplots()
+                ax.imshow(df_wc)
+                st.pyplot(fig)
+            except:
+                # Display error message
+                st.image('error.webp')
+        with col3:
+            try:
+                # heading
+                st.markdown("<h3 style='text-align: center; color: white;'>Negative WordCloud</h3>",unsafe_allow_html=True)
+                
+                # Creating wordcloud of negative words
+                df_wc = helper.create_wordcloud(selected_user, data,-1)
+                fig, ax = plt.subplots()
+                ax.imshow(df_wc)
+                st.pyplot(fig)
+            except:
+                # Display error message
+                st.image('error.webp')
+
+        # Most common positive words
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            try:
+                # Data frame of most common positive words.
+                most_common_df = helper.most_common_words(selected_user, data,1)
+                
+                # heading
+                st.markdown("<h3 style='text-align: center; color: white;'>Positive Words</h3>",unsafe_allow_html=True)
+                fig, ax = plt.subplots()
+                ax.barh(most_common_df[0], most_common_df[1],color='green')
+                plt.xticks(rotation='vertical')
+                st.pyplot(fig)
+            except:
+                # Disply error image
+                st.image('error.webp')
+        with col2:
+            try:
+                # Data frame of most common neutral words.
+                most_common_df = helper.most_common_words(selected_user, data,0)
+                
+                # heading
+                st.markdown("<h3 style='text-align: center; color: white;'>Neutral Words</h3>",unsafe_allow_html=True)
+                fig, ax = plt.subplots()
+                ax.barh(most_common_df[0], most_common_df[1],color='grey')
+                plt.xticks(rotation='vertical')
+                st.pyplot(fig)
+            except:
+                # Disply error image
+                st.image('error.webp')
+        with col3:
+            try:
+                # Data frame of most common negative words.
+                most_common_df = helper.most_common_words(selected_user, data,-1)
+                
+                # heading
+                st.markdown("<h3 style='text-align: center; color: white;'>Negative Words</h3>",unsafe_allow_html=True)
+                fig, ax = plt.subplots()
+                ax.barh(most_common_df[0], most_common_df[1], color='red')
+                plt.xticks(rotation='vertical')
+                st.pyplot(fig)
+            except:
+                # Disply error image
+                st.text('error.webp')
+
+
+
---
+++ helper.py
@@ -0,0 +1,196 @@
+# Importing modules
+import streamlit as st
+from urlextract import URLExtract
+from wordcloud import WordCloud
+import pandas as pd
+from collections import Counter
+import emoji
+
+# Object
+extract = URLExtract()
+
+def fetch_stats2(selected_user,df):
+
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+
+    # fetch the number of messages
+    num_messages = df.shape[0]
+
+    # fetch the total number of words
+    words = []
+    for message in df['message']:
+        words.extend(message.split())
+
+    # fetch number of media messages
+    num_media_messages = df[df['message'] == '<Media omitted>\n'].shape[0]
+
+    # fetch number of links shared
+    links = []
+    for message in df['message']:
+        links.extend(extract.find_urls(message))
+
+    # fetch number of deleted messages
+    num_deleted_messages = df[df['message'] == 'This message was deleted\n'].shape[0]    
+
+
+    return num_messages,len(words),num_media_messages,len(links),num_deleted_messages
+
+def most_busy_users2(df):
+    x = df['user'].value_counts().head()
+    df = round((df['user'].value_counts() / df.shape[0]) * 100, 2).reset_index().rename(
+        columns={'index': 'name', 'user': 'percent'})
+    return x,df
+
+def create_wordcloud2(selected_user,df):
+
+    f = open('stop_hinglish.txt', 'r')
+    stop_words = f.read()
+
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+
+    temp = df[df['user'] != 'group_notification']
+    temp = temp[temp['message'] != '<Media omitted>\n']
+
+    def remove_stop_words(message):
+        y = []
+        for word in message.lower().split():
+            if word not in stop_words:
+                y.append(word)
+        return " ".join(y)
+
+    wc = WordCloud(width=500,height=500,min_font_size=10,background_color='white')
+    temp['message'] = temp['message'].apply(remove_stop_words)
+    df_wc = wc.generate(temp['message'].str.cat(sep=" "))
+    return df_wc
+
+def most_common_words2(selected_user,df):
+
+    f = open('stop_hinglish.txt','r')
+    stop_words = f.read()
+
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+
+    temp = df[df['user'] != 'group_notification']
+    temp = temp[temp['message'] != '<Media omitted>\n']
+
+    words = []
+
+    for message in temp['message']:
+        for word in message.lower().split():
+            if word not in stop_words:
+                words.append(word)
+
+    most_common_df = pd.DataFrame(Counter(words).most_common(20))
+    return most_common_df
+
+def emoji_helper2(selected_user,df):
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+
+    emojis = []
+    for message in df['message']:
+        emojis.extend(c for c in message if c in emoji.EMOJI_DATA)
+
+    emoji_df = pd.DataFrame(Counter(emojis).most_common(len(Counter(emojis))))
+
+    return emoji_df
+
+def monthly_timeline2(selected_user,df):
+
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+
+    timeline = df.groupby(['year', 'month_num', 'month']).count()['message'].reset_index()
+
+    time = []
+    for i in range(timeline.shape[0]):
+        time.append(timeline['month'][i] + "-" + str(timeline['year'][i]))
+
+    timeline['time'] = time
+
+    return timeline
+
+
+def week_activity_map2(selected_user,df):
+
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+
+    return df['day_name'].value_counts()
+
+def month_activity_map2(selected_user,df):
+
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+
+    return df['month'].value_counts()
+
+def activity_heatmap2(selected_user,df):
+
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+
+    user_heatmap = df.pivot_table(index='day_name', columns='period', values='message', aggfunc='count').fillna(0)
+
+    return user_heatmap
+
+# -1 => Negative
+# 0 => Neutral
+# 1 => Positive
+
+
+# Will return percentage of message contributed having k(0/1/-1) sentiment
+def percentage(df,k):
+    df = round((df['user'][df['value']==k].value_counts() / df[df['value']==k].shape[0]) * 100, 2).reset_index().rename(
+        columns={'index': 'name', 'user': 'percent'})
+    return df
+
+# Return wordcloud from words in message
+def create_wordcloud(selected_user,df,k):
+    f = open('stop_hinglish.txt', 'r')
+    stop_words = f.read()
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+    
+    # Remove entries of no significance
+    temp = df[df['user'] != 'group_notification']
+    temp = temp[temp['message'] != '<Media omitted>\n']
+    
+    # Remove stop words according to text file "stop_hinglish.txt"
+    def remove_stop_words(message):
+        y = []
+        for word in message.lower().split():
+            if word not in stop_words:
+                y.append(word)
+        return " ".join(y)
+    # Dimensions of wordcloud
+    wc = WordCloud(width=500,height=500,min_font_size=10,background_color='white')
+    
+    # Actual removing
+    temp['message'] = temp['message'].apply(remove_stop_words)
+    temp['message'] = temp['message'][temp['value'] == k]
+    
+    # Word cloud generated
+    df_wc = wc.generate(temp['message'].str.cat(sep=" "))
+    return df_wc
+
+# Return set of most common words having k(0/1/-1) sentiment
+def most_common_words(selected_user,df,k):
+    f = open('stop_hinglish.txt','r')
+    stop_words = f.read()
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+    temp = df[df['user'] != 'group_notification']
+    temp = temp[temp['message'] != '<Media omitted>\n']
+    words = []
+    for message in temp['message'][temp['value'] == k]:
+        for word in message.lower().split():
+            if word not in stop_words:
+                words.append(word)
+                
+    # Creating data frame of most common 20 entries
+    most_common_df = pd.DataFrame(Counter(words).most_common(20))
+    return most_common_df
